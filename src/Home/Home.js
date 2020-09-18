256cb8592662bc9bdc1ee2b68219334c8af03988
Saurabh Jadhav <GSC-30537@GSLAB.COM>
2020-09-18 10:11:58 +0530

    Done Assignment 1

---
+++ src/Home/Home.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+  // constructor(props){
+    // super(props);
+    // this.state = {};
+  // }
+
+  // componentWillMount(){}
+  // componentDidMount(){}
+  // componentWillUnmount(){}
+
+  // componentWillReceiveProps(){}
+  // shouldComponentUpdate(){}
+  // componentWillUpdate(){}
+  // componentDidUpdate(){}
+
+  render() {
+    return (
+      <div>
+        <h1 className="home">Welcome to Home compenet</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
